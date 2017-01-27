0b50228b2121d386ac3478580523c4dedc04866f
chris-shay <christopher.jackson.shay@gmail.com>
2017-01-27 11:41:56 -0500

    Continue display pattern feature

---
+++ assets/pattern.js
@@ -0,0 +1,11 @@
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   const pattern = document.getElementById("pattern");
+//   for (let i = 0; i < 10; i++) {
+//     debugger
+//     let current = pattern.appendChild("div");
+//     current.style.float = "left";
+//     current.style.width = "50px";
+//     current.stle.background("blue");
+//   }
+// });
