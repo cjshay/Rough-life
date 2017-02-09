601d206b2c6bef8a51eae58ba097930232feee0b
chris-shay <christopher.jackson.shay@gmail.com>
2017-02-08 20:19:44 -0500

    I'm done

--- assets/modal.js
+++ assets/modal.js
@@ -9,13 +9,16 @@ document.addEventListener("DOMContentLoaded", function() {
   const closeModal = document.getElementById("close-modal");
 
   openModal.onclick = function() {
-    body.style.background = "gray";
+    body.style.background = "black";
     modal.style.display = "block";
   };
 
   closeModal.onclick = function() {
     if (event.target !== openModal) {
-      body.style.background = "whitesmoke";
+      // body.style.backgroundimage = "url('images/conway-bg.jpeg')";
+      // body.style.background = "transparent";
+      // body.style.background = "whitesmoke";
+      body.style.background = "black";
       modal.style.display = "none";
     }
   };
